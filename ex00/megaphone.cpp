a84ba9056b9d23fba0e8fbf80482fa26eec47536
Samantha Le Huec <s.lhuec@gmail.com>
2022-11-01 10:32:31 +0100

    add makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,10 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
+/*   By: samantha <samantha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 12:06:08 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/10/25 12:06:09 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/11/01 10:29:34 by samantha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	
+}
