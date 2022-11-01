72b171994457b079de3ebbda7a5b8e8af2f509e4
Samantha Le Huec <s.lhuec@gmail.com>
2022-11-01 14:24:37 +0100

    ex00 ok

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: samantha <samantha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 12:06:08 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/11/01 11:56:06 by samantha         ###   ########.fr       */
+/*   Updated: 2022/11/01 14:23:12 by samantha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,29 @@
 #include <stdio.h>
 #include <string>
 
+void	upp_str(char *arg)
+{
+	std::string upp_str;
+	for (int i = 0; arg[i] != '\0'; i++)
+	{
+		upp_str+=toupper(arg[i]);
+	}
+	std::cout << upp_str;
+}
+
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	if (ac == 1)
 	{
-		std::string Arg;
-		// for (int i = 0; av[1][i] != '\0'; i++)
-		// {
-		// 	std::cout << toupper(av[1][i]);
-		// 	// std::endl;
-		// }
-		// return 0;
-	}
-	else
-	{
-		std::cout << "Need one str as arg." << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 1;
-	}	
+	}
+	upp_str(av[1]);
+	for (int i = 2; i < ac; i++)
+	{
+		std::cout << " ";
+		upp_str(av[i]);
+	}
+	std::cout << std::endl;
+	return 0;
 }
